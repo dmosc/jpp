a68c1644b61956b085f7b96ca2d81b7858e35d18
PatoGuereque <contacto@patoguereque.com>
2022-05-12 20:51:08 -0500

    feat: Memory manager v2 using bit manipulation

--- src/memory-manager.js
+++ src/memory-manager.js
@@ -1,10 +1,52 @@
 const { MEMORY_TYPES, TYPES } = require('./constants');
 
+/**
+ * Memory management
+ * JavaScript has weird integer limits, the "safe" limit
+ * utilizes 54 bits, so we can safely underutilize them
+ * and just use 32 bits.
+ *
+ * Each address is represented by a 32-bit number
+ * 00000000 00000000 00000000 0000000
+ *
+ * The address is divided into parts
+ * The last (left) two bits represent the memory scope
+ * (Bit 31 and 32)
+ * XX000000 00000000 00000000 0000000
+ * 00 = GLOBAL
+ * 01 = LOCAL
+ * 10 = TEMP
+ * 11 = STACK
+ * We can access the memory scope just by simply executing
+ * address >>> 30
+ *
+ * The three next bits represent the memory type
+ * 00XXX000 00000000 00000000 0000000
+ * 000 = INT
+ * 001 = FLOAT
+ * 010 = STRING
+ * 011 = OTHER (objects)
+ * (Last bit is reserved for future types)
+ * We can access the memory scope just by shifting and masking
+ * address >>> 27 & 0x7
+ *
+ * That leaves the rest of the bits available for addresses
+ * 00000XXX XXXXXXXX XXXXXXXX XXXXXXXX
+ * That means we have a total of 2^27 addresses to use,
+ * a total of 134,217,728 usable addresses
+ * We can get the raw address with
+ * address & 0x7FFFFFF
+ */
+
 class Memory {
-  constructor(start, end) {
+  constructor(start, scope, type) {
     this.start = start;
     this.current = start;
-    this.end = end;
+    // our mask is 0x7FFFFFF, so one less is 0x7FFFFFE
+    this.end = start + 0x7fffffe;
+
+    this.scope = scope;
+    this.type = type;
   }
 
   getAddress() {
@@ -21,41 +63,45 @@ class Memory {
 
 class MemoryManager {
   constructor() {
-    this.firstAddress = 10e3;
-    this.lastAddress = 50e3 - 1;
-    this.segments = {
-      [MEMORY_TYPES.GLOBAL]: {
-        [TYPES.INT]: new Memory(this.firstAddress, 14e3 - 1),
-        [TYPES.FLOAT]: new Memory(14e3, 17e3 - 1),
-        [TYPES.STRING]: new Memory(17e3, 20e3 - 1),
-      },
-      [MEMORY_TYPES.LOCAL]: {
-        [TYPES.INT]: new Memory(20e3, 24e3 - 1),
-        [TYPES.FLOAT]: new Memory(24e3, 27e3 - 1),
-        [TYPES.STRING]: new Memory(27e3, 30e3 - 1),
-      },
-      [MEMORY_TYPES.TEMP]: {
-        [TYPES.INT]: new Memory(30e3, 34e3 - 1),
-        [TYPES.FLOAT]: new Memory(34e3, 37e3 - 1),
-        [TYPES.STRING]: new Memory(37e3, 40e3 - 1),
-      },
-      [MEMORY_TYPES.STACK]: {
-        [TYPES.INT]: new Memory(40e3, 44e3 - 1),
-        [TYPES.FLOAT]: new Memory(44e3, 47e3 - 1),
-        [TYPES.STRING]: new Memory(47e3, this.lastAddress),
-      },
-    };
+    this.supportedScopes = [
+      MEMORY_TYPES.GLOBAL,
+      MEMORY_TYPES.LOCAL,
+      MEMORY_TYPES.TEMP,
+      MEMORY_TYPES.STACK,
+    ];
+    this.scopeLookup = this.supportedScopes.reduce(
+      (prev, curr, i) => Object.assign(prev, { [curr]: i }),
+      {}
+    );
+
+    this.supportedTypes = [TYPES.INT, TYPES.FLOAT, TYPES.STRING];
+    this.typeLookup = this.supportedTypes.reduce(
+      (prev, curr, i) => Object.assign(prev, { [curr]: i }),
+      {}
+    );
+
+    this.segments = this.supportedScopes.map((scope, scopeIndex) => {
+      const scopeBits = scopeIndex << 3;
+      return this.supportedTypes.map((type, typeIndex) => {
+        return new Memory((scopeBits | typeIndex) << 27, scope, type);
+      });
+    });
+
+    console.log(this.segments);
   }
 
   getMemorySegment(memoryType, dataType) {
-    return this.segments[memoryType][dataType];
+    return this.segments[this.scopeLookup[memoryType]][
+      this.typeLookup[dataType]
+    ];
+  }
+
+  getScope(address) {
+    return this.supportedScopes[address >>> 30];
   }
 
   getType(address) {
-    const remainder = address % this.firstAddress;
-    if (remainder >= 7e3) return TYPES.STRING;
-    if (remainder >= 4e3) return TYPES.FLOAT;
-    return TYPES.INT;
+    return this.supportedTypes[(address >>> 27) & 0x7];
   }
 }
 
